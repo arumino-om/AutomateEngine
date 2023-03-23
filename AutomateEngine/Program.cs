6295964f340aa2fb906e447b902c77b6d5c22186
Arumino <102341161+arumino-om@users.noreply.github.com>
2023-03-23 18:54:08 +0900

    add NLua and implement basic interpreter

--- AutomateEngine/Program.cs
+++ AutomateEngine/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NLua;
+using NLua.Exceptions;
 
-Console.WriteLine("Hello, World!");
+namespace AutomateEngine;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        using var lua = new Lua();
+        if (args.Length > 0)
+        {
+            if (File.Exists(args[0])) lua.DoFile(args[0]);
+            else
+            {
+                WriteError($"File not exist: {args[0]}");
+                Interpreter(lua);
+            }
+        }
+        else Interpreter(lua);
+    }
+
+    private static void Interpreter(Lua lua)
+    {
+        Console.WriteLine($"Automate Engine Interpreter (AE 1.0 / {lua["_VERSION"]})");
+        while (true)
+        {
+            Console.Write(">> ");
+            var expression = Console.ReadLine();
+            if (string.IsNullOrEmpty(expression)) continue;
+            if (expression == "exit") break;
+
+            try
+            {
+                lua.DoString($"print({expression})");
+            }
+            catch (LuaScriptException le)
+            {
+                WriteError(le.Message);
+            }
+        }
+        Console.WriteLine("Bye");
+    }
+
+    private static void WriteError(string value)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(value);
+        Console.ResetColor();
+    }
+}
