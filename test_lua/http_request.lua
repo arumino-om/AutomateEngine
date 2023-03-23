664da91760be88c5125343e6f80799687d365a14
Arumino <102341161+arumino-om@users.noreply.github.com>
2023-03-24 01:13:05 +0900

    add a sample script for http

---
+++ test_lua/http_request.lua
@@ -0,0 +1,19 @@
+--[[
+    HTTP request sample - Automate Engine
+]]
+
+-- create http instance and setup request headers
+req = http:create();
+headers = {
+    UserAgent = "Automate Engine"
+}
+
+-- send HTTP request using the send_request() method
+result = req:send_request("http://localhost", "get", nil, headers); -- uri, method, requestBody, headers
+
+-- if any error occurred, property has_syserror will be set to true
+if result.has_syserror then
+    print(result.error);
+else
+    print("Ok");
+end
